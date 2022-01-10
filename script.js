2adb2c6e4750351efdb44e3bba4d465d82092b43
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2022-01-10 13:26:20 +0100

    milestone 1

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 */
+
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            emails: []
+        },
+        created() {
+            this.getMail();
+        },
+        methods: {
+            //creo la funzione per generare 10 indirizzi mail
+            getMail: function () {
+                for (let i = 0; i < 10; i++) {
+                    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                        .then((response) => {
+                            //pusho l'indirizzo nell'array
+                            this.emails.push(response.data.response);
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        })
+                }
+            }
+        },
+    }
+)
